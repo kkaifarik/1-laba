49723dfa20467e9e30a1fdee355eacd564479228
kkaifarik <y.krivonosov@list.ru>
2023-02-28 15:47:02 +0300

    фиурные скобки 7 строке
    Signed-off-by: kkaifarik <y.krivonosov@list.ru>

--- 2.4.py
+++ 2.4.py
@@ -4,7 +4,7 @@ b=[]
 def coins_1(a):
     print("Введите количество монет с разным номиналом:")
     kol_1=int(input())
-    a = [int(input("Введите номинал монеты: ".format(i))) for i in range(kol_1)]
+    a = [int(input("Введите номинал монеты: {}".format(i))) for i in range(kol_1)]
     a.sort()
     return(a)
 def coins_2(b):
